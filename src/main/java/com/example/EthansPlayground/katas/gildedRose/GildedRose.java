3e99b95abc5f8975f8c5e7f1cb1c06549f16f28f
Ethan <ethanbinya@aol.com>
2026-04-19 03:52:44 -0400

    completed guilded rose kata 1

--- src/main/java/com/example/EthansPlayground/katas/gildedRose/GildedRose.java
+++ src/main/java/com/example/EthansPlayground/katas/gildedRose/GildedRose.java
@@ -1,88 +1,68 @@
 package com.example.EthansPlayground.katas.gildedRose;
 
 class GildedRose {
-  private final static String SPECIAL_SULFURAS = "Sulfuras, Hand of Ragnaros";
-  private final static String SPECIAL_AGED_BRIE = "Aged Brie";
-  private final static String SPECIAL_BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
-  private final Item[] items;
+    private final static String SPECIAL_SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private final static String SPECIAL_AGED_BRIE = "Aged Brie";
+    private final static String SPECIAL_BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    private final Item[] items;
 
-  public GildedRose(Item[] items) {
-    this.items = items;
-  }
-
-  public void updateQuality() {
-    for (Item item : items) {
-      thing(item);
-
-      if (item.name.equals(SPECIAL_SULFURAS)) {
-        continue;
-      }
-
-      if (item.name.equals(SPECIAL_AGED_BRIE)) {
-        if (item.quality < 50) {
-          item.quality++;
-        }
-
-        item.sellIn--;
-
-        if (item.sellIn < 0) {
-          if (item.quality < 50) {
-            item.quality++;
-          }
-        }
-
-        continue;
-      }
-
-
-      if (item.name.equals(SPECIAL_AGED_BRIE)
-        || item.name.equals(SPECIAL_BACKSTAGE_PASS)) {
-        if (item.quality < 50) {
-          item.quality++;
-
-          if (item.name.equals(SPECIAL_BACKSTAGE_PASS)) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality++;
-              }
-            }
-
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality++;
-              }
-            }
-          }
-        }
-        item.sellIn--;
-      }
-
-      if (item.sellIn < 0) {
-        if (item.name.equals(SPECIAL_BACKSTAGE_PASS)) {
-          item.quality = 0;
-        } else if (item.name.equals(SPECIAL_AGED_BRIE)) {
-          if (item.quality < 50) {
-            item.quality++;
-          }
-        }
-      }
+    public GildedRose(Item[] items) {
+        this.items = items;
     }
-  }
 
-  private void thing(Item item) {
-    switch (item.name) {
-      case SPECIAL_SULFURAS:
-        break;
-      case SPECIAL_AGED_BRIE:
-        break;
-      case SPECIAL_BACKSTAGE_PASS:
-        break;
-      default:
+    public void updateQuality() {
+        for (Item item : items) {
+            processItem(item);
+        }
+    }
+
+    private void processItem(Item item) {
+        switch (item.name) {
+            case SPECIAL_SULFURAS:
+                return;
+            case SPECIAL_AGED_BRIE:
+                processAgedItem(item);
+                break;
+            case SPECIAL_BACKSTAGE_PASS:
+                processBackstagePass(item);
+                break;
+            default:
+                processDefaultItem(item);
+                break;
+        }
+    }
+
+    private void processDefaultItem(Item item) {
         int depreciateBy = item.sellIn > 0 ? 1 : 2;
         item.quality -= depreciateBy;
         item.quality = Math.max(0, item.quality);
         item.sellIn--;
-        break;
     }
-  }
+
+    private void processAgedItem(Item item) {
+        item.sellIn--;
+        int appreciateBy = item.sellIn >= 0 ? 1 : 2;
+        item.quality += appreciateBy;
+        item.quality = Math.min(50, item.quality);
+    }
+
+    private void processBackstagePass(Item item) {
+        int appreciateBy;
+
+        if (item.sellIn <= 5) {
+            appreciateBy = 3;
+        } else if (item.sellIn <= 10) {
+            appreciateBy = 2;
+        } else {
+            appreciateBy = 1;
+        }
+
+        item.quality += appreciateBy;
+        item.quality = Math.min(50, item.quality);
+
+        item.sellIn--;
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
 }
