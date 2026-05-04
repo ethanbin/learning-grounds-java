2aa995aae5af6038b112dfbe5d013f99a164c0b0
Ethan <ethanbinya@aol.com>
2026-05-04 00:57:43 -0400

    bowling kata

---
+++ src/main/java/com/example/EthansPlayground/katas/bowlingGame/BowlingGame.java
@@ -0,0 +1,5 @@
+package com.example.EthansPlayground.katas.bowlingGame;
+
+public class BowlingGame {
+
+}
---
+++ src/test/java/com/example/EthansPlayground/katas/BowlingGame/BowlingGameTest.java
@@ -0,0 +1,20 @@
+package com.example.EthansPlayground.katas.BowlingGame;
+
+import com.example.EthansPlayground.katas.bowlingGame.BowlingGame;
+import com.example.EthansPlayground.katas.gildedRose.GildedRose;
+import com.example.EthansPlayground.katas.gildedRose.GildedRose2;
+import com.example.EthansPlayground.katas.gildedRose.Item;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BowlingGameTest {
+  public BowlingGame getBowlingGame() {
+    return new BowlingGame();
+  }
+
+  @Test
+  void testItemUpdatesDownBy1() {
+
+  }
+}
