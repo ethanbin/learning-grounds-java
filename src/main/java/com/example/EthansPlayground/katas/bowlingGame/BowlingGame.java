7333a3b4899b365218d62222692f7c90291f26e6
Ethan <ethanbinya@aol.com>
2026-05-04 01:09:54 -0400

    bowling kata work

--- src/main/java/com/example/EthansPlayground/katas/bowlingGame/BowlingGame.java
+++ src/main/java/com/example/EthansPlayground/katas/bowlingGame/BowlingGame.java
@@ -1,5 +1,16 @@
 package com.example.EthansPlayground.katas.bowlingGame;
 
-public class BowlingGame {
+public abstract class BowlingGame {
 
+    /**
+     * void roll(int) is called each time the player rolls a ball. The argument is the number of pins knocked down
+     * @param pinsKnockedDown
+     */
+    public abstract void roll(int pinsKnockedDown);
+
+    /**
+     * int score() returns the total score for that game
+     * @return score
+     */
+    public abstract int score();
 }
